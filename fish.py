ea879e954bb015e6466f23298a25d4ac167cf5cf
jrey16 <jreynolds19@mytrinityhs.org>
2017-07-06 13:53:08 -0500

    Creating fish.py

---
+++ fish.py
@@ -0,0 +1,26 @@
+from turtle import *
+import math
+
+# Name your Turtle.
+t = Turtle()
+
+# Set Up your screen and starting position.
+setup(500,300)
+x_pos = -250
+y_pos = -150
+t.setposition(x_pos, y_pos)
+
+### Write your code below:
+t.forward(25)
+t.right(95)
+t.forward(200)
+t.left(90)
+t.forward(380)
+t.left(95)
+t.forward(200)
+t.right(95)
+t.forward(15)
+t.left(134)
+t.forward(265)
+# Close window on click.
+exitonclick()
